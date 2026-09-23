2d5d9447a50b9fb568ff1b1ee44b8635f18be8f6
MikhailKhetagurov <misha7688@gmail.com>
2026-09-23 12:24:54 +0000

    Update Flux lesson

--- cluster/versions.tf
+++ cluster/versions.tf
@@ -5,7 +5,7 @@ terraform {
   # Замініть ОРГАНІЗАЦІЯ на ім'я своєї організації.
   # Якщо працюєте локально зі станом у файлі — закоментуйте весь блок cloud.
   cloud {
-    organization = "ОРГАНІЗАЦІЯ"
+    organization = "Open5151"
 
     workspaces {
       tags = ["hillel-eks"]
